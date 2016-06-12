93b553cabb6fd3ac7468fc1fb626b226b557f5ca
alexanderpinchao <alexander.pinchao@gmail.com>
2016-06-12 18:50:39 -0500

    Update clear-parentesis.py

--- clear-parentesis.py
+++ clear-parentesis.py
@@ -1,19 +1,36 @@
+#importamos el paquete de expresiones regulares 
 import re
-file=open("quito.couch")
+#abrimos el archivo con la informacion 
+file=open("quito.json")
+#asignamos la informacion a una variable para ser utilizada 
 texto=file.read()
+#cerramos el archivo 
 file.close()
-#print texto
+#generamos la expresion regular para recolectar las coordenadas y la fecha  
 patron = re.compile(ur'created_at":([^,]+),"f[^j]*[^\[]+([^}]+)')
+#damos el formato a la variable donde ingrsamos los datos 
 text='u'+texto
+#procesamos la informacion y asignamos las variables a una tupla de dos columnas con los valores de coordenadas en 
+#la primera columna y los de la fecha de creacion en la segunda 
 m=re.findall(patron,texto)
-negras=("{","}","[","]")
-print len(m)
+#recorremos los valores de la tupla 
 for x in m:
+  #comvertimos la tupla a una cadena para su procesamiento y la asignamos a una variable 
   key=str(x)	
+  #eliminamos los [ de la variable 
   key=key.replace("[","")
+   #eliminamos los ] de la variable 
   key=key.replace("]","")
+   #eliminamos los ( de la variable 
   key=key.replace("(","")
+   #eliminamos los ) de la variable 
   key=key.replace(")","")
+   #eliminamos los " de la variable 
   key=key.replace('"',"")
+   #eliminamos los ' de la variable 
   key=key.replace("'","")
+   #imprimimos los valores de la variable 
   print key 
+  
+  #los valores se imprimiran en consola en caso de desear un archivo de texto usar el modificador >>nomnbreArchivo.txt 
+  #escrito a continuacion de la orden de compilacion ejemplo (~#pyhton archi.py>>archivo.txt)
