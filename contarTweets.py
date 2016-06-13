dcbe6ffeb464925888b402a9d877c19d33cfc2d3
alexanderpinchao <alexander.pinchao@gmail.com>
2016-06-12 19:29:11 -0500

    Update and rename contar.py to contarTweets.py

---
+++ contarTweets.py
@@ -0,0 +1,17 @@
+#archivo creado para contar el numero de tweets en un archivo json
+import re
+#abrimos el archivo json 
+file=open("pichincha.json")
+#asignamos los datos encontrados en una variable 
+texto=file.read()
+#cerramos el archivo
+file.close()
+#creamos la expresion regular que nos permitira saber el numero de tweets en el archivo 
+patron = re.compile(ur'created_at":([^,]+),"f[^j]*[^\[]+([^}]+)')
+#damos formato al texto 
+text='u'+texto
+#comparamos los datos de la variable con la expresion regular y lo agregamos a una tupla
+m=re.findall(patron,texto)
+#imprime el valor de la longitud de la tupla 
+print len(m)
+
