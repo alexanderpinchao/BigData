e132777a312da89cfe97fdb0895615fc59774d65
alexanderpinchao <alexander.pinchao@gmail.com>
2016-06-12 22:44:33 -0500

    Update twe1.py

--- twe1.py
+++ twe1.py
@@ -1,22 +1,32 @@
+#codigo creado por: Montufar Diego
+#agreagmos el paquete de expresiones regulares
 import re
+#agregamos el paquete para manejar couchdb
 import couchdb
+#agregamos los paquetes de tweepy
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+#Agregamos el paquete para manejar objetos json
 import json
 
 
 ###API ########################
+#Credenciales de acceso a twitter
 ckey = "nZPXeBQmpn7lVh3k7RhfqdX9K"
 csecret = "uOsWTomsSntwHAz9vkXLcTASdmaW6yykWoxDQGSyT4yttlBLrx"
 atoken = "731096320363114496-cWWceoZ0OpeaMPHYanzDSFLi4FDIWgW"
 asecret = "ENS0HkrqCrhaFZX56AJloTYFigsvmNCyBQxk83szFaPmo"
 #####################################
-
-class listener(StreamListener):     
+#creacion del listener
+class listener(StreamListener):
+#obtenemos la informacion de la API conserniente al tweet
     def on_data(self, data):
+    	#agregamos esta informacion a un objeto json
         dictTweet = json.loads(data)
+        #creamos un bloque de control para comprobar si el tweet ya existe o no
         try:
+        	#agregamos la id al objeto a ingresar
             dictTweet["_id"] = str(dictTweet['id'])
 #generacion de la expresion regular con el termino a buscar 
             p=re.compile(ur'"Quito"')
@@ -32,13 +42,15 @@ class listener(StreamListener):
 #en caso de que el termino aparesca en el texto agrega el documento en la base de datos 
                doc = db.save(dictTweet)
         except:
+        	#en caso de existir el tweet imprimimos un mensaje 
             print "Already exists"
             pass
+           #cambiamos el valor de la bandera a verdadero
         return True
     
     def on_error(self, status):
         print status
-        
+        #agregamos los valores a nuestro listener para su consumo 
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener())
